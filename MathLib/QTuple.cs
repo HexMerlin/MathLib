de37f744d99d72cb41e1d21063aed080fafd25fd
HexMerlin <alec-sweden@hotmail.com>
2024-10-20 13:11:58 +0200

    Updates

--- MathLib/QTuple.cs
+++ MathLib/QTuple.cs
@@ -1,21 +1,22 @@
-﻿//using System.Diagnostics;
-//using System.Numerics;
-
-//namespace MathLib;
+﻿namespace MathLib;
 
 
-//public class QTuple
-//{
-//    public int Base { get; }
+public class QTuple
+{
+    public int Base { get; }
+
+    public Q A { get; }
+
+    public Q B { get; }
+
+    public QTuple(Q q, int base_)
+    {
+        this.Base = base_;
+        Qp qp = new Qp(q, base_);
+
+        A = (q + qp.Generator) / 2;
+        B = (q - qp.Generator) / 2;
+    }
 
 
-//    public QTuple(Q q, int base_)
-//    {
-//        this.Base = base_;
-//        Qp qp = new Qp(q, base_);
-
-
-//    }
-
-
-//}
+}
