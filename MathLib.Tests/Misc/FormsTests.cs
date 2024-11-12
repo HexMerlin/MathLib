1577651ac08f9795a031ea35cf8cb6dc64bd0e1f
HexMerlin <alec-sweden@hotmail.com>
2024-11-12 21:03:09 +0100

    Add ModMinAbs method, Forms class, and FormsTests
    Added ModMinAbs method to BigIntegerExtensions for modulo with smallest absolute value. Introduced Forms class with methods for non-adjacent and balanced binary forms. Created FormsTests class to verify these methods, including helper methods and range tests.

---
+++ MathLib.Tests/Misc/FormsTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLib.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace MathLib.Misc.Tests;
+
+[TestClass()]
+public class FormsTests
+{
+    private static void AssertNonAdjacentFormIsExpected(BigInteger expected)
+    {      
+        BigInteger weight = BigInteger.One;
+        BigInteger sum = 0;
+        bool prevWasZero = true;
+        foreach (int c in expected.ToNonAdjacentForm())
+        {
+            if (c == 0)
+                prevWasZero = true;
+            else if (c is -1 or 1)
+            {
+                Assert.IsTrue(prevWasZero, $"Invalid NAF sequence: contains consecutive non-zero digits");
+                sum += c * weight;
+                prevWasZero = false;
+            }
+            else 
+                Assert.Fail($"Invalid NAF digit for number {expected}: {c}");
+            weight *= 2;
+        }
+        Assert.AreEqual(expected, sum, $"The sum of the NAF digits does not equal the expected number {expected}");
+    }
+
+    public static void AssertBalancedBinaryIsExpected(BigInteger expected)
+    {
+        BigInteger weight = BigInteger.One;
+        BigInteger sum = 0;
+    
+        var debug = expected.ToBalancedBinary().ToArray();
+
+        foreach (int c in expected.ToBalancedBinary())
+        {
+            if (c is -1 or 1)
+                sum += c * weight;
+            else Assert.Fail($"Invalid balanced binary digit for number {expected}: {c}");
+            weight *= 2;
+        }
+
+        Assert.AreEqual(expected, sum, $"The sum of the balanced binary digits does not equal the expected number {expected}");
+    }
+
+    [TestMethod()]
+    public void ToNonAdjacentFormTest_ForRange_ReturnsCorrect()
+    {
+        for (BigInteger expected = -31; expected <= 31; expected++)
+            AssertNonAdjacentFormIsExpected(expected);
+    }
+
+    [TestMethod()]
+    public void ToBalancedBinary_ForRange_ReturnsCorrect()
+    {
+        for (BigInteger expected = -31; expected <= 31; expected+=2)
+            AssertBalancedBinaryIsExpected(expected);
+    }
+  
+}  
---
+++ MathLib/Misc/Forms.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace MathLib.Misc;
+public static class Forms
+{
+    public static IEnumerable<int> ToNonAdjacentForm(this BigInteger integer)
+    {
+        BigInteger k = BigInteger.Abs(integer);
+        int sign = integer.Sign;
+
+        while (k != 0)
+        {
+            int z;
+            if (k % 2 != 0)
+            {
+                z = 2 - (int)(k % 4);
+                if (z == 2)
+                    z = -1;
+                k -= z;
+            }
+            else
+            {
+                z = 0;
+            }
+            yield return sign * z;
+            k /= 2;
+        }
+    }
+
+
+    public static int BalancedBit(BigInteger number, int index, bool isLastDigit)
+    {
+        BigInteger div = BigInteger.One << (index + 2);
+        BigInteger mod = number.ModMinAbs(div);
+        return isLastDigit ? mod.Sign : -mod.Sign;
+    }
+
+    public static IEnumerable<int> ToBalancedBinary(this BigInteger integer)
+    {
+        if (integer.IsEven)
+            throw new ArgumentException("The balanced binary form can only represent odd numbers.");
+
+        BigInteger rem = integer;
+        for (int index = 0; ; index++)
+        {
+            int digit = BalancedBit(integer, index, rem.Abs().IsOne);
+            yield return digit;
+            rem -= digit;
+            rem /= 2;
+            if (rem == 0)
+                break;
+        }
+    }
+}
